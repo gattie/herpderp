2175742e303b25635950a27fe9a45e706e6d42ba
Matt Gaspar <mgaspar@godaddy.com>
2019-06-13 15:28:36 -0700

    Refactor our tests
    We no longer need the test to verify that fizzbuzz is calable, and the
    two tests we have so far are very similar, so we can create a utility
    function to remove code duplication.

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,13 +1,12 @@
 from fizzbuzz import fizzbuzz
 import pytest
 
-def test_fizzbuzz_is_callable():
-    fizzbuzz(1)
+def check_fizzbuzz(value, expected_value):
+    ret_val = fizzbuzz(value)
+    assert ret_val == expected_value
 
 def test_return_1_when_passed_1():
-    ret_val = fizzbuzz(1)
-    assert ret_val == "1"
+    check_fizzbuzz(1, "1")
 
 def test_return_2_when_passed_2():
-    ret_val = fizzbuzz(2)
-    assert ret_val == "2"
+    check_fizzbuzz(2, "2")
