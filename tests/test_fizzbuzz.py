734d306b8ef884d0ba8b4773e0e0ae187bc688b6
Matt Gaspar <mgaspar@godaddy.com>
2019-06-13 15:24:56 -0700

    Add test for returning "2"
    We've now added a new test to return "2" when 2 is passed in to
    fizzbuzz. This test is currently failing, and we'll fix it in the next
    commit.

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -7,3 +7,7 @@ def test_fizzbuzz_is_callable():
 def test_return_1_when_passed_1():
     ret_val = fizzbuzz(1)
     assert ret_val == "1"
+
+def test_return_2_when_passed_2():
+    ret_val = fizzbuzz(2)
+    assert ret_val == "2"
